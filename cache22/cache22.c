26686318cf000c7d4fbf0c54d5ae6e7ab2e44339
PieBerlin <brightongorova@gmail.com>
2026-03-20 10:46:16 +0300

    Implement  server and client connection

--- cache22/cache22.c
+++ cache22/cache22.c
@@ -2,13 +2,77 @@
 #include"cache22.h"
 
 bool server_continuation;
-void mainloop(int16 port){
+bool child_continuation;
+void zero(int8 *buf, int16 size)
+{
+    int8 *p;
+    int16 n;
+    for (n = 0, p = buf; n < size; n++,p++){
+        *p = 0;
+    }
+    return;
+}
+
+void childloop(Client *cli){
+    sleep(1);
+    return;
+}
+
+void mainloop(int s){
+    struct sockaddr_in cli;
+    int s2;
+    int32 len;
+    char *ip;
+    int16 port;
+    Client *client;
+    pid_t pid;
+
+    s2 = accept(s, (struct sockaddr *)&cli,(unsigned int *)&len);
+    if (s2<0){
+       // sleep(1);
+        return;
+    }
+
+    port = (int16)htons((int)cli.sin_port);
+    ip = inet_ntoa(cli.sin_addr);
+    printf("Connection from  %s:%d\n",ip,port);
+
+    client = (Client *)malloc(sizeof(struct s_client));
+    assert(client);
+
+    zero((int8 *)client,sizeof(struct s_client));
+    client->s = s;
+    client->port = port;
+    strncpy(client->ip, ip, 15);
+
+    pid = fork();
+    if(pid){
+        free(client);
+
+        return;
+    }
+    else{
+        dprintf(s2,"100 Connected to cache22 server\n...");
+        child_continuation = true;
+        while(child_continuation){
+        childloop(client);
+
+        }
+        close(s2);
+        free(client);
+        return ;
+    }
+
+    //return ;
+}
+
+int initserver(int16 port){
     struct sockaddr_in sock;
     int s;
 
     sock.sin_family = AF_INET;
     sock.sin_port = htons(port);
-    sock.sin_addr.s_addr = INADDR_ANY; // inet_addr(HOST);
+    sock.sin_addr.s_addr = inet_addr(HOST); 
     s = socket(AF_INET, SOCK_STREAM, 0);
 
     assert(s>0);
@@ -23,11 +87,16 @@ void mainloop(int16 port){
     {
         assert_perror(errno);
     }
+    
+    printf("Server listenning on %s:%d\n",HOST,port);
+
+    return s;
 }
 
 int main(int argc, char *argv[]){
     char *sport;
     int16 port;
+    int s;
 
     if(argc<2){
         sport = PORT;
@@ -38,10 +107,17 @@ int main(int argc, char *argv[]){
 
     port = (int16)atoi(sport);
 
+    s=initserver(port);
+
     server_continuation = true;
     while(server_continuation){
-        mainloop(port);
+        mainloop(s);
+
+        
     }
+
+    printf("Shutting down... \n");
+    close(s);
 
     return 0;
  }
--- cache22/cache22.h
+++ cache22/cache22.h
@@ -14,17 +14,34 @@
 #include<errno.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
+#include <stddef.h>
+#include<stdarg.h>
 
 
 
 #define HOST "127.0.0.1"
 #define PORT "12049"
 
+
+
 typedef unsigned int int32;
 typedef unsigned short int int16;
 typedef unsigned char int8;
 
-void mainloop(int16);
+struct s_client{
+    int s;
 
+    char ip[16];
+    int16 port;
+};
+typedef struct s_client Client;
+
+// functions
+void zero(int8*, int16);
+void childloop(Client*);
+void mainloop(int);
+int initserver(int16);
 int main(int, char **);
+
+
 #endif
